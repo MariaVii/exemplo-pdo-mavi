68462791d9b51804163d381e58b0c2cfccee67fa
MariaVii <mav.andraade@gmail.com>
2022-12-01 14:51:48 +0000

    Preparação Da Base De Dados

---
+++ vendor/assets/sql/dados_iniciais.sql
@@ -0,0 +1,12 @@
+INSERT INTO generos(id, nome) VALUES
+(1, 'alta fantasia'),
+(2, 'romance'),
+(3, 'comédia'),
+(4, 'ficção');
+
+INSERT INTO livros(id, titulo, id_genero) VALUES
+(1, 'Harry Potter e a Pedra Filosofal', 2),
+(2, 'O amor nos tempos do cólera', 1),
+(3, 'A Troca', 3),
+(4, 'A culpa é das estrelas', 4);
+
